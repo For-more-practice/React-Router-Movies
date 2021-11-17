701120ddc48c2593bbd2c52c3db8f50dac315977
justinmavity <justinmavity1021@gmail.com>
2021-11-16 21:29:35 -0600

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,31 +13,38 @@ export default function App() {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
-    saved.push(id);
-    setSaved({ saved });
+    if (movieList.id === id && !saved.includes(id)) {
+      setSaved([...saved, movieList.id])
+    }
   };
 
   return (
     <div>
-      <SavedList list={saved} />
-      <Route path="/" component={MovieList} />
-      <Route path="/movies/:id">
-        <Movie key={movieList.id} />
-      </Route>
+      <SavedList list={saved} addToSaved={addToSavedList} />
+      <Switch>
+        <Route path={"/movies/:id"}>
+          <Movie />
+        </Route>
+        <Route path={"/"}>
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
+      <div>Replace this Div with your Routes</div>
     </div>
   );
 }
